1d702e2458fcceb089f8f17af8e83d361184f004
woooyoung <axdsw121@gmail.com>
2021-11-20 11:24:47 +0900

    주소록 프로그램3 - DB에서 목록 가져와 출력하기

--- addr/AddrMain.java
+++ addr/AddrMain.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddrMain {
@@ -26,6 +27,18 @@ public class AddrMain {
 				
 				insertAddress(name, address, phone);
 				
+			} else if(cmd.equals("list")) {
+				ArrayList<Addr> addrList = selectAddresses();
+				
+				System.out.println("========= 주소록 목록 =========");
+				for(int i = 0; i < addrList.size(); i++) {
+					Addr addr = addrList.get(i);
+					System.out.println("번호 : " + addr.getIdx());
+					System.out.println("이름 : "  + addr.getName());
+					System.out.println("주소 : "  + addr.getAddress());
+					System.out.println("전화번호 : " + addr.getPhone());
+					System.out.println("==============================");
+				}
 			}
 			
 		}
@@ -73,4 +86,55 @@ public class AddrMain {
 		}
 	}
 
+	
+	public static ArrayList<Addr> selectAddresses() {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		String url = "jdbc:mysql://localhost:3306/a1?serverTimezone=UTC";
+		String user = "root";
+		String pass = "";
+		
+		ArrayList<Addr> addrList = new ArrayList<>();
+			
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, pass);
+			stmt = conn.createStatement();
+
+			String sql = "SELECT * FROM t_addr";
+			
+			System.out.println("sql : " + sql);
+			rs = stmt.executeQuery(sql);
+			
+			
+			while(rs.next()) { // 다음 로우로 이동 다음이 있으면 true 반환, 없으면 false 반환
+				
+				int idx = rs.getInt("idx");
+				String name = rs.getString("name");
+				String address = rs.getString("address");
+				String phone = rs.getString("phone");
+				
+				Addr a1 = new Addr(idx, name, address, phone);				
+				addrList.add(a1);
+			}
+						
+			if(rs != null) {
+				rs.close();				
+			}
+			if(stmt != null) {
+				stmt.close();				
+			}
+			if(conn != null) {
+				conn.close();				
+			}
+			
+		} catch (Exception e) {
+			System.out.println("접속 시도중 문제 발생!!");
+		}
+		
+		return addrList;
+	}
 }
