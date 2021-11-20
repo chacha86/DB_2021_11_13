317aabd326f8d27c65e94a1b4a9ee6e0f1fd358b
woooyoung <axdsw121@gmail.com>
2021-11-20 10:50:37 +0900

    주소록 프로그램2 - 주소 DB에 추가

--- addr/AddrMain.java
+++ addr/AddrMain.java
@@ -4,12 +4,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class AddrMain {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);		
+		
+		while(true) {
+			System.out.print("명령어를 입력해주세요 : ");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("add")) {
+				System.out.print("이름 : ");
+				String name = sc.nextLine();
+				System.out.print("주소 : ");
+				String address = sc.nextLine();
+				System.out.print("전화번호 : ");
+				String phone = sc.nextLine();
+				
+				insertAddress(name, address, phone);
+				
+			}
+			
+		}
+		
+	}
 
-		// 자동임포트 : 컨트로 + (좌)쉬프트 + O
+	public static void insertAddress(String name, String address, String phone) {
+
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
@@ -28,22 +52,25 @@ public class AddrMain {
 			// 3. Statement 생성
 			stmt = conn.createStatement();
 
-			// 4. SQL 처리하고 결과 ResultSet에 받아오기
-			String sql = "SELECT * FROM t_addr";
-			rs = stmt.executeQuery(sql);
+			// 4. SQL 처리
+			String sql = "INSERT INTO t_addr SET `name` = '" + name + "', address = '" + address + "', phone = '" + phone + "'";
+			System.out.println("sql : " + sql);
+			stmt.executeUpdate(sql);
 
-			while (rs.next()) {
-				System.out.println(rs.getInt("idx"));
-				System.out.println(rs.getString("name")); // 문자열로 리턴
-				System.out.println(rs.getString("address")); // 문자열로 리턴
-				System.out.println(rs.getString("phone")); // 문자열로 리턴
-				System.out.println("========================");
+			// 5. 작업 다 했으면 자원 반납
+			if(rs != null) {
+				rs.close();				
 			}
-
+			if(stmt != null) {
+				stmt.close();				
+			}
+			if(conn != null) {
+				conn.close();				
+			}
+			
 		} catch (Exception e) {
 			System.out.println("접속 시도중 문제 발생!!");
 		}
-
 	}
 
 }
