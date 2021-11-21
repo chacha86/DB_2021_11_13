ae6849a0d411e9b973c0e20ae3efc185c1c99479
woooyoung <axdsw121@gmail.com>
2021-11-21 10:38:56 +0900

    DBUtil 만들고 게시물 추가(add) DB 반영

---
+++ db_query/board_schema.sql
@@ -0,0 +1,21 @@
+# board DB 생성
+CREATE DATABASE board;
+
+# board DB 사용
+USE board;
+
+# article table 생성
+CREATE TABLE article (
+    idx INT(5) PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(200) NOT NULL,
+    `body` TEXT NOT NULL,
+    nickname VARCHAR(50),
+    regDate DATETIME # 게시물 등록일
+);
+
+DESC article;
+
+INSERT INTO article SET title = 'aaa', `body` = 'bbb', nickname = 'ccc', regDate = '20211121103300';
+
+SELECT *
+FROM article;
